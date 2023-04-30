e11ffef6f16dc21eae5382cb56171db061fcdee8
GaryL123 <126824940+GaryL123@users.noreply.github.com>
2023-04-29 22:47:48 -0400

    Update Blockchain.java

--- src/core/Blockchain.java
+++ src/core/Blockchain.java
@@ -1,9 +1,10 @@
 package core;
 
-import java.util.*;
 import network.*;
+import java.security.PublicKey;
+import java.util.*;
 
-public class Blockchain{
+public class Blockchain {
     private List<Block> chain;
     private List<Transaction> pendingTransactions;
     private List<Transaction> blockTransactions = new ArrayList<Transaction>();
@@ -17,7 +18,7 @@ public class Blockchain{
 
     private Block createGenesisBlock() {
         List<Transaction> emptyTransactions = new ArrayList<Transaction>();
-        Block genBlock = new Block("0", emptyTransactions);
+        Block genBlock = new Block("0", emptyTransactions, null, null);
         genBlock.setGenBlockHash();
         return genBlock;
     }
@@ -53,7 +54,7 @@ public class Blockchain{
         pendingTransactions.add(transaction);
     }
 
-    public void acceptPendingTransaction(Transaction transaction, int index, int nodeId) {
+    public void acceptPendingTransaction(Transaction transaction, int index, byte[] signature, String publicString , int nodeId) {
         // If the blockTransactions list doesn't exist or is full, create a new list
         if (blockTransactions == null || blockTransactions.size() >= 2) {
             blockTransactions = new ArrayList<>();
@@ -65,7 +66,8 @@ public class Blockchain{
         //when max transaction capacity is reached, create a new block from the queue
         //and empty the queue
         if (blockTransactions.size() >= 2) {
-            Block newBlock = new Block(getLatestBlock().getHash(), blockTransactions);
+            Block newBlock = new Block(getLatestBlock().getHash(), blockTransactions, signature, publicString);
+            newBlock.setSignatureString();
             addBlock(newBlock, nodeId);
             blockTransactions = null; // Set to null to create a new list for the next block
             System.out.println("added new block");
