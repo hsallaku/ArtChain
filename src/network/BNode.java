4be6ef18ff1dc76cbd0d2b32ff4c9993c440635a
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2023-04-30 08:37:51 -0400

    Update BNode.java

--- src/network/BNode.java
+++ src/network/BNode.java
@@ -1,198 +1,119 @@
 package network;
 
-import core.*;
-import utils.*;
-import java.util.ArrayList;
-import java.util.List;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.annotations.Expose;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import core.*;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.*;
-import java.security.interfaces.ECPrivateKey;
+import java.lang.reflect.Type;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class BNode {
+public class SynchronizationSimulator implements Runnable {
 
-    @Expose
-    private int id;
-    @Expose
-    private String username;
-    @Expose
-    private String password;
-    @Expose
-    private String status;
-    @Expose
-    private String filePath;
-    @Expose
-    private String privateKeyString;
-    @Expose
-    private String publicKeyString;
-    private transient PrivateKey privateKey;
-    private transient PublicKey publicKey;
-    private Blockchain blockchain;
+    private int nodesCount;
 
-    public BNode() {
-        id = 0;
-        username = "";
-        password = "";
-        status = "";
-        filePath = "";
-        privateKeyString = "";
-        publicKeyString = "";
-        blockchain = new Blockchain();
-    }
-
-    public BNode(String username, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        this.username = username;
-        this.password = StringUtil.applySha256(password);
-        this.status = "s"; // stands for standard node, validators will have the letter v
-        blockchain = new Blockchain();
-        //generate the keyPair for new nodes
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
-        SecureRandom random = SecureRandom.getInstanceStrong();
-        keyPairGenerator.initialize(256, random);
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-        this.publicKey = keyPair.getPublic();
-        this.privateKey = (ECPrivateKey) keyPair.getPrivate();
-        privateKeyString = Base64.getEncoder().encodeToString(privateKey.getEncoded());
-        publicKeyString = Base64.getEncoder().encodeToString(publicKey.getEncoded());
-        // Check if the nodes.json file exists, and if not, create an empty one
-        File usersFile = new File("users.json");
-        if (!usersFile.exists()) {
-            try ( FileWriter writer = new FileWriter(usersFile)) {
-                writer.write("[]");
-            } catch (IOException e) {
+    @Override
+    public void run() {
+        Random random = new Random();
+        while (true) {
+            nodesCount = getTotalNumberOfNodes();
+            if (nodesCount >= 3) {
+                int nodeId1 = random.nextInt(nodesCount) + 1;
+                int nodeId2 = random.nextInt(nodesCount) + 1;
+                if (nodeId1 != nodeId2) {
+                    try {
+                        compareNodes(nodeId1, nodeId2);
+                    } catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException | InvalidKeySpecException | UnsupportedEncodingException ex) {
+                        Logger.getLogger(SynchronizationSimulator.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
 
-        // Read the list of nodes from the JSON file
-        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
-        List<BNode> userList;
-        try ( FileReader reader = new FileReader(usersFile)) {
-            userList = gson.fromJson(reader, new TypeToken<List<BNode>>() {
-            }.getType());
-            if (userList == null) {
-                userList = new ArrayList<>();
+    private void compareNodes(int nodeId1, int nodeId2) throws InvalidKeyException, NoSuchAlgorithmException, SignatureException, InvalidKeySpecException, UnsupportedEncodingException {
+        Blockchain blockchain1 = BlockchainIO.loadBlockchain(String.format("blockchain%d.json", nodeId1));
+        Blockchain blockchain2 = BlockchainIO.loadBlockchain(String.format("blockchain%d.json", nodeId2));
+        int length1 = blockchain1.getLength();
+        int length2 = blockchain2.getLength();
+        int size1 = blockchain1.getPendingTransactions().size();
+        int size2 = blockchain2.getPendingTransactions().size();
+
+        BNode node1 = UsersIO.loadUsers("users.json").get(nodeId1 - 1);
+        BNode node2 = UsersIO.loadUsers("users.json").get(nodeId2 - 1);
+
+        if (length1 != length2) {
+            if (length1 > length2) {
+                Block testBlock = blockchain1.getLatestBlock();
+                if (testBlock.verifySignature()) {
+                    System.out.println("Signature verification passed");
+                    BlockchainIO.saveBlockchain(String.format("blockchain%d.json", nodeId2), blockchain1);
+                    System.out.printf("%s updated their blockchain based on %s's%n", node2.getUsername(), node1.getUsername());
+                } else {
+                    System.out.println("Signature verification failed");
+                }
+            } else {
+                Block testBlock = blockchain1.getLatestBlock();
+                if (testBlock.verifySignature()) {
+                    System.out.println("Signature verification passed");
+                    BlockchainIO.saveBlockchain(String.format("blockchain%d.json", nodeId1), blockchain2);
+                    System.out.printf("%s updated their blockchain based on %s's%n", node1.getUsername(), node2.getUsername());
+                } else {
+                    System.out.println("Signature verification failed");
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            userList = new ArrayList<>();
+        } else {
+            String hash1 = blockchain1.getLatestBlock().getHash();
+            String hash2 = blockchain2.getLatestBlock().getHash();
+
+            if (hash1.equals(hash2)) {
+                System.out.printf("%s is in sync with %s%n", node1.getUsername(), node2.getUsername());
+            } else {
+                verifyNode(node1);
+                verifyNode(node2);
+            }
+        }
+    }
+
+    private void verifyNode(BNode node) {
+        System.out.printf("Verifying node %d, known as %s%n", node.getId(), node.getUsername());
+    }
+
+    // Return the number of nodes located in the database
+    public int getTotalNumberOfNodes() {
+        File usersFile = new File("users.json");
+        Gson gson = new Gson();
+        List<BNode> users;
+
+        if (usersFile.exists()) {
+            try ( FileReader reader = new FileReader(usersFile)) {
+                Type userListType = new TypeToken<ArrayList<BNode>>() {
+                }.getType();
+                users = gson.fromJson(reader, userListType);
+            } catch (IOException e) {
+                e.printStackTrace();
+                users = new ArrayList<>();
+            }
+        } else {
+            users = new ArrayList<>();
         }
 
-        // Assign an ID to the new node based on the last entry
-        int lastId = userList.isEmpty() ? 0 : userList.get(userList.size() - 1).getId();
-        this.id = lastId + 1;
-        this.filePath = String.format("blockchain%d.json", id);
-
-        //Creates new json file for the node
-        BlockchainIO.saveBlockchain(filePath, blockchain);
-
-        // Add the new node to the list
-        BNode currentUser = this;
-        userList.add(currentUser);
-
-        // Save the updated list of nodes to the JSON file
-        try ( BufferedWriter writer = new BufferedWriter(new FileWriter(usersFile))) {
-            gson.toJson(userList, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.printf("Node %d connected to network\n", id);
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = StringUtil.applySha256(password);
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public String getFilePath() {
-        return this.filePath;
-    }
-
-    public Blockchain getBlockchain() {
-        return blockchain;
-    }
-
-    public void setBlockchain(Blockchain blockchain) {
-
-        this.blockchain = blockchain;
-    }
-
-    public PrivateKey getPrivateKey() {
-        return this.privateKey;
-    }
-
-    public String getPrivateString() {
-        return privateKeyString;
-    }
-
-    public PublicKey getPublicKey() {
-        return this.publicKey;
-    }
-
-    public String getPublicString() {
-        return publicKeyString;
-    }
-
-    public void restoreKeysFromLoad() throws NoSuchAlgorithmException, InvalidKeySpecException {
-        byte[] privateBytes = Base64.getDecoder().decode(getPrivateString());
-        byte[] publicBytes = Base64.getDecoder().decode(getPublicString());
-        privateKey = KeyFactory.getInstance("EC").generatePrivate(new PKCS8EncodedKeySpec(privateBytes));
-        publicKey = KeyFactory.getInstance("EC").generatePublic(new X509EncodedKeySpec(publicBytes));
-    }
-
-    public byte[] signBlock() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
-        Signature signature = Signature.getInstance("SHA256withECDSA");
-        signature.initSign(getPrivateKey());
-        return signature.sign();
-    }
-
-    public boolean verifyBlock(PublicKey publicKey, byte[] signature) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        Signature verify = Signature.getInstance("SHA256withECDSA");
-        verify.initVerify(publicKey);
-        return verify.verify(signature);
+        return users.size();
     }
 
 }
