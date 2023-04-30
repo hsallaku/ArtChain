c211d31cd53cf3837699ef5cd4d7e8ed871efda8
GaryL123 <126824940+GaryL123@users.noreply.github.com>
2023-04-29 22:47:04 -0400

    Update Block.java

--- src/core/Block.java
+++ src/core/Block.java
@@ -1,7 +1,16 @@
 package core;
 
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.List;
 import utils.StringUtil;
 
@@ -9,13 +18,18 @@ public class Block {
     private String hash;
     private String previousHash;
     private String timeStamp;
+    private String publicString;
+    private String signatureString;
+    private transient byte[] signature;
     private List<Transaction> transactions;
 
-    public Block(String previousHash, List<Transaction> transactions) {
+    public Block(String previousHash, List<Transaction> transactions, byte[] signature, String publicString) {
         this.previousHash = previousHash;
         this.timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         this.transactions = transactions;
         this.hash = calculateHash();
+        this.signature = signature;
+        this.publicString = publicString;
     }
 
 
@@ -36,6 +50,14 @@ public class Block {
     public void setGenBlockHash(){
         this.hash = "4427766ec2a0fa4f182b3d5c88c8b9d6730ce0e54d5e5d09bc7d2b7bf01b7df3";
     }
+    public void setSignatureString()
+    {
+        signatureString = Base64.getEncoder().encodeToString(signature);
+    }
+    public String getSignatureString()
+    {
+        return signatureString;
+    }
 
     // Getters and setters
     public String getHash() {
@@ -52,6 +74,23 @@ public class Block {
 
     public List<Transaction> getTransactions() {
         return transactions;
+    }
+    public byte[] getSignature()
+    {
+        return signature;
+    }
+    public String getPublicString()
+    {
+        return publicString;
+    }
+    public boolean verifySignature() throws InvalidKeyException, NoSuchAlgorithmException, SignatureException, InvalidKeySpecException
+    {
+    byte[] publicBytes = Base64.getDecoder().decode(publicString);
+    PublicKey publicKey = KeyFactory.getInstance("EC").generatePublic(new X509EncodedKeySpec(publicBytes));
+    Signature verify = Signature.getInstance("SHA256withECDSA");
+    verify.initVerify(publicKey);
+    return verify.verify(signature);
+    
     }
 
     @Override
